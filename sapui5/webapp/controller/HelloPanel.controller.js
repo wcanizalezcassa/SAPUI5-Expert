ba9adaffb59887f3f8ed0af1c912583ff5dbbaaf
wcanizalezcassa <walter.canizalez@grupocassa.com>
2023-01-14 04:05:24 +0000

    Laboratorio 15

--- sapui5/webapp/controller/HelloPanel.controller.js
+++ sapui5/webapp/controller/HelloPanel.controller.js
@@ -1,7 +1,8 @@
 // @ts-nocheck
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/base/Log"
 
 ],
     /**
@@ -9,13 +10,23 @@ sap.ui.define([
      * @param {typeof sap.ui.core.mvc.Controller} Controller 
      * @param {typeof sap.m.MessageToast} MessageToast 
      */
-    function (Controller, MessageToast) {
+    function (Controller, MessageToast,Log) {
         "use strict";
 
         return Controller.extend("logaligroup.sapui5.controller.HelloPanel", {
 
             onInit: function () {
 
+            },
+
+            onBeforeRendering: function() {
+                window.message = 'Log message -obBeforeRendering';
+                Log.info(window.message);
+                Log.error(window.message);
+            },
+
+            onAfterRendering: function() {
+                debugger;
             },
             onShowHello: function () {
                 //read text from i18n model
